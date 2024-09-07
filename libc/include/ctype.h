03199a250706a42b89702b71e462d3937d2ff1bf
Yash Verma <yashverma2304@gmail.com>
2024-09-07 03:07:56 -0400

    Initial Commit

---
+++ libc/include/ctype.h
@@ -0,0 +1,27 @@
+#ifndef _CTYPE_H
+#define _CTYPE_H
+
+#include <sys/defs.h>
+
+__CDECLS_BEGIN
+
+int isalnum(int ch);
+int isalpha(int ch);
+int isblank(int ch);
+int iscntrl(int ch);
+int isdigit(int ch);
+int isgraph(int ch);
+int islower(int ch);
+int isprint(int ch);
+int ispunct(int ch);
+int isspace(int ch);
+int isupper(int ch);
+int isxdigit(int ch);
+
+int tolower(int ch);
+int toupper(int ch);
+int toascii(int ch);
+
+__CDECLS_END
+
+#endif // _CTYPE_H
